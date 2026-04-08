3ca96f7cbf4c61fa9a29b3dee3433f450469a14b
PetchAuisui <siwarpatauisui@gmail.com>
2026-04-08 23:48:51 +0700

    Boolean State

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
   { id: 2, name: 'Jane Smith', gender:"female", age: 25 },
   { id: 3, name: 'Bob Johnson', gender:"male", age: 40 },
   ]);
+  const [show, setShow] = useState(true)
 
   console.table(data)
   return (
@@ -15,13 +16,14 @@ function App() {
     <h1>Learning React</h1>
     <h2>Welcome, {name}!</h2>
     <p>You are {age} years old.</p>
-    <button onClick={()=>setAge(age + 1)}>add-age</button>
-    <button onClick={()=>setAge(age - 1)}>subtract-age</button>
-    <button onClick={()=>setAge(30)}>reset</button>
+    <button onClick={()=>setAge(age + 1)}>Add</button>
+    <button onClick={()=>setAge(age - 1)}>Subtract</button>
+    <button onClick={()=>setAge(30)}>Reset</button>
     <h3>จำนวนผู้ใช้งาน: {data.length} คน </h3>
+    <button onClick={()=>setShow(!show)}>{show ? 'Hide' : 'Show'}</button>
     <u>
-      {data.map((item) => (
-      <li key ={item.id}>{item.name}</li>
+      {show && data.map((item) => (
+      <li key ={item.id}> {item.id} | {item.name} | {item.gender} | {item.age}</li>
       ))}
     </u>
   </section>
