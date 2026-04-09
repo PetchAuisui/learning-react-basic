16633ebc9bc9c7ac8d92e23bfe83b48f1b666d5f
PetchAuisui <siwarpatauisui@gmail.com>
2026-04-10 00:48:46 +0700

    Dynamic Style

--- src/components/PersonList.jsx
+++ src/components/PersonList.jsx
@@ -8,6 +8,9 @@ function PersonList() {
     { id: 1, name: "John Doe", gender: "male", age: 30 },
     { id: 2, name: "Jane Smith", gender: "female", age: 25 },
     { id: 3, name: "Bob Johnson", gender: "male", age: 40 },
+    { id: 4, name: "Alice Williams", gender: "female", age: 35 },
+    { id: 5, name: "Emily Davis", gender: "female", age: 22 }
+    
   ]);
   const [show, setShow] = useState(true);
   const myStyle = {
@@ -25,7 +28,7 @@ function PersonList() {
       <ul>
         {show &&
           data.map((item) => (
-            <li key={item.id}>
+            <li key={item.id} style={{borderStyle:"solid",borderColor:item.gender=="male" ? "blue" : "pink"}}>
               <img src={item.gender == "male" ? boy : girl} width={50} height={50}/>
               <p> ชื่อ: {item.name} อายุ: {item.age} เพศ: {item.gender}</p>
               <div className="control">
