0519dde6382553007bb03a9d0e0148c34d7b06d7
PetchAuisui <siwarpatauisui@gmail.com>
2026-04-15 22:42:40 +0700

    onChange Event

--- src/components/AddForm.jsx
+++ src/components/AddForm.jsx
@@ -1,12 +1,15 @@
 import "./AddForm.css";
+import { useState } from "react";
 
 export default function Addform() {
+    const [name, setName] = useState("");
+    const [gender, setGender] = useState("ชาย");
     return (
         <section className="container">
             <form>
                 <label>ชื่อประชากร</label>
-                <input type="text" name="name" />
-                <select name="gender">
+                <input type="text" value={name} onChange={(e)=>setName(e.target.value)}/>
+                <select value={gender} onChange={(e)=>setGender(e.target.value)}>
                     <option>ชาย</option>
                     <option>หญิง</option>
                 </select>
