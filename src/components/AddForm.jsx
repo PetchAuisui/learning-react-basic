8b6423ce57d6ec2cd17939632081e6db7bf58a49
PetchAuisui <siwarpatauisui@gmail.com>
2026-04-15 22:53:38 +0700

    onSubmit Event

--- src/components/AddForm.jsx
+++ src/components/AddForm.jsx
@@ -4,9 +4,19 @@ import { useState } from "react";
 export default function Addform() {
     const [name, setName] = useState("");
     const [gender, setGender] = useState("ชาย");
+
+    function saveData(e){
+        e.preventDefault()
+        const person = {
+            name: name,
+            gender: gender
+        }
+        console.log(person);
+    }
+
     return (
         <section className="container">
-            <form>
+            <form onSubmit={saveData}>
                 <label>ชื่อประชากร</label>
                 <input type="text" value={name} onChange={(e)=>setName(e.target.value)}/>
                 <select value={gender} onChange={(e)=>setGender(e.target.value)}>
